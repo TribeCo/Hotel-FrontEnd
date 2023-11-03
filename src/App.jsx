c9f1f90f16c9b8884f1becbb832efee6f5c4f6b1
reza buzarjemehri <dev.reza21@gmail.com>
2023-11-03 14:15:57 +0330

    convert to rtl

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { prefixer } from "stylis";
+import createCache from "@emotion/cache";
+
 import { Button } from "@mui/material";
+
+// Create theme
+const theme = createTheme({
+	direction: "rtl",
+});
+
+// Create rtl cache
+const cacheRtl = createCache({
+	key: "muirtl",
+	stylisPlugins: [prefixer, rtlPlugin],
+});
 
 const App = () => {
 	return (
-		<>
-			<p>Hotel Transylvania</p>
-			<Button variant="contained">دکمه</Button>
-		</>
+		<CacheProvider value={cacheRtl}>
+			<ThemeProvider theme={theme}>
+				<HelmetProvider>
+					<Helmet>
+						<title>Hotel Transylvania</title>
+					</Helmet>
+					<div>
+						<Button variant="contained">دکمه</Button>
+					</div>
+				</HelmetProvider>
+			</ThemeProvider>
+		</CacheProvider>
 	);
 };
 
