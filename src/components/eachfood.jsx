e66148ed8bec448e50d5751546f7f794c89af3a8
Parsavazifeh <p_vazifeh@comp.iust.ac.ir>
2023-12-11 11:40:04 +0330

    -

---
+++ src/components/eachfood.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './eachfood.css'; // Import your CSS file here
+import './tailwind.css';
+import bk from '../assets/eachfood.png';
+
+const Eachfood = () => {
+  return (
+    <div className="flex flex-col h-full lg:flex-row">
+      {/* Left side (picture) */}
+      <div className="w-full h-auto lg:w-1/2">
+        <img src={bk} alt="Your eachfoodimage" className="w-full h-auto" />
+      </div>
+  
+      {/* Right side (content) */}
+      <div className="w-full eachfoodcontainer lg:w-1/2">
+        <h1 className="eachfoodroom-info mt-4">مشخصات غذا</h1>
+        <div className="eachfoodrow mx-2">
+          <div className="relative flex flex-col">
+            <label htmlFor="eachfooditem" className="eachfoodlabel absolute bottom-10 left-10"> قیمت</label>
+            <input type="text" placeholder="Input 1" className="eachfooditem2 w-1/3 p-2 border rounded-md mx-2" />
+          </div>
+        </div>
+        <div className="eachfoodrow">
+          <div className="relative flex flex-col">
+            <label htmlFor="eachfooditem" className="eachfoodlabel absolute bottom-54 left-3/4"> توضیحات</label>
+            <textarea className="eachfoodtext px-3 py-2" placeholder="Enter your text here..."></textarea>
+          </div>
+        </div>
+        <div className="eachfoodrow">
+          <button className="eachfoodbutton">رزرو غذا</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Eachfood;
