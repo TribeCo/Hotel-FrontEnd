544d001948988f850f0a674debe437dd48dc31c1
taa <TahaM8000@gmail.com>
2024-01-16 21:35:19 +0330

    config server

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./tailwind.css";
-import "./landing.css";
+// import "./landing.css";
 import { Group } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 
